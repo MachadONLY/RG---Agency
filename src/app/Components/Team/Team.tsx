5a03b31af7a6446307b8ed7ec06057b83f6f26c6
MachadONLY <rm551570@fiap.com.br>
2024-05-08 16:18:00 -0300

    team

--- src/app/Components/Team/Team.tsx
+++ src/app/Components/Team/Team.tsx
@@ -1,77 +1,67 @@
-import Image from "next/image";
+"use client";
+
 import React from "react";
-import Profile from "../../../../public/assets/Profile.svg";
 import { FaFacebook, FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa";
+import Profile from "../../../../public/assets/Profile.svg";
+import Image from "next/image";
 
-const Team = () => {
+interface TeamMemberProps {
+  name: string;
+  role: string;
+  bio: string;
+}
+
+const TeamMember: React.FC<TeamMemberProps> = ({ name, role, bio }) => {
   return (
-    <div className="w-full h-auto">
-      <div className="w-full my-[800px] md:my-[400px] gap-20 lg:my-20 mx-auto max-w-[1240px] h-full flex flex-col md:flex-row justify-center items-center">
-        <div className="w-[275px] h-[60%] bg-zinc-100 shadow-2xl rounded-2xl flex flex-col items-center">
-          <div className="w-[85%] flex flex-col items-center">
-            <Image
-              src={Profile}
-              alt="Profile CEO"
-              width={150}
-              className="shadow-xl rounded-full mb-6 shadow-gray-500 my-4 border-4 border-zinc-200"
-            />
-            <h1 className="text-xl uppercase font-semibold">Ruan Gabriel</h1>
-            <span className="text-[#6265eb] my-1">CEO</span>
-            <p className="text-justify my-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Dignissimos adipisci a laborum voluptatum, soluta non nam maxime
-              qui dolore nulla!
-            </p>
+    <div className="w-[275px] h-[60%] bg-zinc-100 shadow-2xl rounded-2xl flex flex-col items-center">
+      <div className="w-[85%] flex flex-col items-center">
+        <Image
+          src={Profile}
+          alt={`Profile of ${name}`}
+          width={150}
+          className="shadow-xl rounded-full mb-6 shadow-gray-500 my-4 border-4 border-zinc-200"
+        />
+        <h1 className="text-xl uppercase font-semibold">{name}</h1>
+        <span className="text-[#6265eb] my-1 uppercase">{role}</span>
+        <p className="text-justify my-2">{bio}</p>
 
-            <div className="flex gap-3 my-8">
-              <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
-                <FaInstagram />
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
-                <FaTwitter />
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
-                <FaFacebook />
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
-                <FaTiktok />
-              </div>
-            </div>
-          </div>
+        <div className="flex gap-3 my-8">
+          <SocialIcon icon={<FaInstagram />} />
+          <SocialIcon icon={<FaTwitter />} />
+          <SocialIcon icon={<FaFacebook />} />
+          <SocialIcon icon={<FaTiktok />} />
         </div>
+      </div>
+    </div>
+  );
+};
 
-        <div className="w-[275px] h-[60%] bg-zinc-100 shadow-2xl rounded-2xl flex flex-col items-center">
-          <div className="w-[85%] flex flex-col items-center">
-            <Image
-              src={Profile}
-              alt="Profile CEO"
-              width={150}
-              className="shadow-xl rounded-full mb-6 shadow-gray-500 my-4 border-4 border-zinc-200"
-            />
-            <h1 className="text-xl uppercase font-semibold">Gabriel Machado</h1>
-            <span className="text-[#6265eb] my-1 uppercase">CO-Founder</span>
-            <p className="text-justify my-2">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Dignissimos adipisci a laborum voluptatum, soluta non nam maxime
-              qui dolore nulla!
-            </p>
+interface SocialIconProps {
+  icon: React.ReactNode;
+}
 
-            <div className="flex gap-3 my-8">
-              <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
-                <FaInstagram />
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
-                <FaTwitter />
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
-                <FaFacebook />
-              </div>
-              <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
-                <FaTiktok />
-              </div>
-            </div>
-          </div>
-        </div>
+const SocialIcon: React.FC<SocialIconProps> = ({ icon }) => {
+  return (
+    <div className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300 hover:bg-[#5651e5] hover:text-zinc-50">
+      {icon}
+    </div>
+  );
+};
+
+const Team: React.FC = () => {
+  return (
+    <div className="w-full h-screen">
+      <div className="w-full gap-20 my-[800px] lg:my-0 md:my-[400px] mx-auto max-w-[1240px] h-full flex flex-col md:flex-row justify-center items-center">
+        <TeamMember
+          name="Ruan Gabriel"
+          role="CEO"
+          bio="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos adipisci a laborum voluptatum, soluta non nam maxime qui dolore nulla!"
+        />
+        <TeamMember
+          name="Gabriel Machado"
+          role="Co-founder"
+          bio="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos adipisci a laborum voluptatum, soluta non nam maxime qui dolore nulla!"
+        />
       </div>
     </div>
   );
--- src/app/Components/Valores/Valores.tsx
+++ src/app/Components/Valores/Valores.tsx
@@ -1,35 +1,45 @@
-import React from 'react'
-import { FaLinkedin, FaMedal, FaRegLightbulb, FaUserFriends } from 'react-icons/fa'
+import React from 'react';
+import { FaLinkedin, FaMedal, FaRegLightbulb, FaUserFriends } from 'react-icons/fa';
 
-const Valores = () => {
+const Valores: React.FC = () => {
   return (
     <div className='w-full h-[500px]'>
-        <div className='mx-auto max-w-[1240px] w-full h-full my-[500px] md:my-[800px] lg:my-20'>
-            <div className='mx-auto lg:mx-0 w-[85%] lg:w-full h-full'>
-                <p className='text-2xl font-semibold text-justify lg:w-[50%]'>Na RG Agency, somos apaixonados por ajudar negócios locais a alcançarem <span className='text-zinc-500'>todo o seu potencial através do poder do marketing digital.</span> Conheça mais sobre nossos valores</p>
-                <div className='flex flex-col lg:flex-row gap-12 my-20'>
-                    <div className='flex flex-col gap-7 border-b-2 pb-8 border-zinc-900 lg:border-b-0 lg:border-r-2 lg:pr-8'>
-                        <FaMedal size={45} />
-                        <h1 className='font-bold text-xl'>Excelência</h1>
-                        <p className='w-full'>Priorizamos qualidade e a eficácia em todas as nossas entregas</p>
-                    </div>
-
-                    <div className='flex flex-col gap-7 border-b-2 pb-8 border-zinc-900 lg:border-b-0 lg:border-r-2 lg:pr-8'>
-                        <FaUserFriends size={45} />
-                        <h1 className='font-bold text-xl'>Foco no Cliente</h1>
-                        <p className='w-full'>Colocamos as necessidades dos nossos clientes em primeiro lugar</p>
-                    </div>
-
-                    <div className='flex flex-col gap-7 border-b-2 pb-8 border-zinc-900 lg:border-b-0'>
-                        <FaRegLightbulb size={45} />
-                        <h1 className='font-bold text-xl'>Inovação</h1>
-                        <p className='w-full'>Valorizando inovação e a criatividade como catalisadores do sucesso</p>
-                    </div>
-                </div>
-            </div>
+      <div className='mx-auto max-w-[1240px] w-full h-full my-[500px] md:my-[800px] lg:my-20'>
+        <div className='mx-auto lg:mx-0 w-[85%] lg:w-full h-full'>
+          <p className='text-2xl font-semibold text-justify lg:w-1/2'>
+            Na RG Agency, ajudamos negócios locais a alcançarem <span className='text-zinc-500'>todo o seu potencial através do poder do marketing digital.</span>  Conheça mais sobre nossos valores
+          </p>
+          <div className='flex flex-col lg:flex-row gap-12 my-20'>
+            {valores.map((valor, index) => (
+              <div key={index} className={`flex flex-col gap-7 border-b-2 pb-8 border-zinc-900 lg:border-b-0 ${index === 2 ? '' : 'lg:border-r-2 lg:pr-8'}`}>
+                {valor.icon}
+                <h1 className='font-bold text-xl'>{valor.title}</h1>
+                <p className='w-full'>{valor.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Valores
+const valores = [
+  {
+    icon: <FaMedal size={45} />,
+    title: 'Excelência',
+    description: 'Priorizamos qualidade e eficácia em todas as entregas.',
+  },
+  {
+    icon: <FaUserFriends size={45} />,
+    title: 'Foco no Cliente',
+    description: 'Colocamos as necessidades dos clientes em primeiro lugar.',
+  },
+  {
+    icon: <FaRegLightbulb size={45} />,
+    title: 'Inovação',
+    description: 'Valorizamos inovação e criatividade como catalisadores do sucesso.',
+  },
+];
+
+export default Valores;
